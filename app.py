f45fa1ccf88f178bf31a9a54f2d75989c2754efe
shreyesss <64736777+shreyesss@users.noreply.github.com>
2022-01-10 19:37:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,154 +1,169 @@
-from re import DEBUG, sub
-from flask import Flask, render_template, request, redirect, send_file, url_for,jsonify
-
-from werkzeug.utils import secure_filename
-import os
+# Core Pkgs
+# import ee
+from torch.serialization import load
+import streamlit as st
 import cv2
-import subprocess
-import torch
-from cnnlstm import CNNLSTM
+import numpy as np
+# import folium
+import os
+from PIL import Image
+# from selenium import webdriver
+import pandas as pd
+# import plotly.graph_objects as go
+# import plotly.express as px
+import datetime
+from dateutil.relativedelta import relativedelta
+import tempfile
 from spatial_transforms import (
     Compose, Normalize, Scale, CenterCrop, CornerCrop, MultiScaleCornerCrop,
     MultiScaleRandomCrop, RandomHorizontalFlip, ToTensor)
+
+
+import numpy as np
+import torch
 import torch.nn.functional as F
-from PIL import Image
-import sys
+from cnnlstm import CNNLSTM
+import matplotlib.pyplot as plt
+import os 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
-app = Flask(__name__)
-uploads_dir = os.path.join(app.instance_path, 'uploads')
-os.makedirs(uploads_dir, exist_ok=True)
+st.set_page_config(layout="wide")
+if torch.cuda.is_available() :
+    device = "cuda"
+else :
+    device = "cpu"
 
 
-print("----Loading Model-------")
-checkpoint = torch.load("./ckpt.pth", map_location='cpu')
-model = CNNLSTM(num_classes=2)
-model.load_state_dict(checkpoint['state_dict'])
-class_to_idx = {"goal" : 1, "no-goal" : 0}
-mean =  [114.7748, 107.7354 , 99.4750]
-std =   [38.7568578 , 37.88248729,40.02898126 ]
-device = torch.device("cpu")
-print(class_to_idx)
-idx_to_class = {}
-for name, label in class_to_idx.items():
-    idx_to_class[label] = name
-print("----Model Loaded-------")
+   
 
+def load_model():
+    
+    print("----Loading Model-------")
+    checkpoint = torch.load("./model-2.pth", map_location='cpu')
+    model = CNNLSTM(num_classes=2)
+    model.load_state_dict(checkpoint['state_dict'])
+    class_to_idx = {"goal": 1, "no-goal": 0}
+    mean = [114.7748, 107.7354, 99.4750]
+    std = [1,1,1]
+    print(class_to_idx)
+    idx_to_class = {}
+    for name, label in class_to_idx.items():
+        idx_to_class[label] = name
+    print("----Model Loaded-------")
+    return model.to(device)
 
-
-# def resume_model(opt, model):
-#     """ Resume model 
-#     """
-#     checkpoint = torch.load(opt.resume_path, map_location='cpu')
-#     model.load_state_dict(checkpoint['state_dict'])
-
-
-def predict(clip, model):
-
-    norm_method = Normalize(mean,std)
-
+def predict(clip, model,thresh):
+  
+    mean = [114.7748, 107.7354, 99.4750]
+    std = [1,1,1] 
+    norm_method = Normalize(mean, [1, 1, 1])
     spatial_transform = Compose([
-        Scale((224,224)),
-        #Scale(int(opt.sample_size / opt.scale_in_test)),
-        #CornerCrop(opt.sample_size, opt.crop_position_in_test),
+        Scale((224, 224)),
         ToTensor(1), norm_method
     ])
     if spatial_transform is not None:
         # spatial_transform.randomize_parameters()
         clip = [spatial_transform(img) for img in clip]
 
+    # print(clip[0])
     clip = torch.stack(clip, dim=0)
-    clip = clip.unsqueeze(0)
+    clip = clip.unsqueeze(0).to(device)
     with torch.no_grad():
-        print(clip.shape)
+        # print(clip.shape)
         outputs = model(clip)
         outputs = F.softmax(outputs)
-    scores, idx = torch.topk(outputs, k=1)
-    mask = scores > 0.6
-    preds = idx[mask]
-    return preds
+        print(outputs.cpu().numpy())
+    if outputs[:,-1] > thresh :
+       return 1
+    else:
+       return 0
 
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
-
-######################Doesnt work########################################
-
-@app.route("/detect", methods=['POST'])
-def detect():
-    if not request.method == "POST":
-        return
-    print(request.files)
-    video = request.files['video']
-    # print(request)
-    # # video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
-    # print(video)
-    return jsonify("hello")
-
+def detect(video,model,thresh):
+    fps = video.get(5)
+    num_frames = 2 * (fps // 16  + 1)
+    print("fps : " , fps)
+    out = []
+    outs = []
     clip = []
+
     frame_count = 0
     while True:
         ret, img = video.read()
         if frame_count == 16:
-            # print(len(clip))
-            preds = predict(clip, model)
-            draw = img.copy()
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            if preds.size(0) != 0:
-                # print(idx_to_class[preds.item()])
-                cv2.putText(draw, idx_to_class[preds.item(
-                )], (100, 100), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
-
+            out.append(predict(clip,model,thresh))
             frame_count = 0
             clip = []
-
-        #img = cv2.resize(img, (224,224))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        #img = Image.fromarray(img.astype('uint8'), 'RGB')
-        img = Image.fromarray(img)
+        if len(out) == num_frames or not ret:
+            pred = max(out)
+            outs.append(pred)
+            out = []
+        if not ret :
+            return  outs
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.uint8)
+        img = Image.fromarray(img.astype('uint8'), 'RGB')
         clip.append(img)
         frame_count += 1
-    jsonify(result=result, probability=pred_proba)
 
+def plot(out):
+    fig = plt.figure(figsize = (10, 5))
+    if type(out) == int :
+        out = [out]
+    base = [str((2*(i+1))) + "sec" for i in range(len(out))]
+    print(out)
+    # creating the bar plot
+    plt.bar(base, out, color ='maroon',
+            width = 0.4)
+    
+    plt.xlabel("time (sec)")
+    plt.ylabel("Shot or Not")
+    ax = plt.gca()
+    ax.set_ylim([0,1])
+    plt.plot()
+    plt.show()
+    return fig
    
-   
-
-# @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     if request.method == 'POST':
-#         # Get the image from post request
-#         img = base64_to_pil(request.json)
-
-#         # Save the image to ./uploads
-#         # img.save("./uploads/image.png")
-
-#         # Make prediction
-#         preds = model_predict(img, model)
-
-#         # Process your result for human
-#         pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-#         pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-#         result = str(pred_class[0][0][1])               # Convert to string
-#         result = result.replace('_', ' ').capitalize()
-        
-#         # Serialize the result, you can add additional fields
-#         return jsonify(result=result, probability=pred_proba)
-
-#     return None
 
 
-@app.route('/return-files', methods=['GET'])
-def return_file():
-    obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
-    print(loc)
-    try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
-        # return send_from_directory(loc, obj)
-    except Exception as e:
-        return str(e)
+def main():
+    """BasketBall Pointer Detection App"""
+    st.title("MTX Hackolympics")
 
-if __name__ == "__main__":
-    app.run(debug=True,port = 6969)
+    activities = ["Inference", "About" , "Metrics"]
+    choice = st.sidebar.selectbox("Select Activty", activities)
+
+    if choice == 'Inference':
+        st.subheader("Video Detection")
+
+        options = ["Upload Video"]
+        selection = st.selectbox("Select Option", options)
+
+        if selection == 'Upload Video':
+            video_file = st.file_uploader("Upload Video", type=['mp4', 'mov'])
+
+            if video_file is not None:
+                tfile = tempfile.NamedTemporaryFile(delete=False)
+                tfile.write(video_file.read())
+                vf = cv2.VideoCapture(tfile.name)
+                model = load_model()
+                out = detect(video=vf,model=model,thresh=0.5)
+                print(out)
+                fig = plot(out)
+
+                st.text("Original Video")
+                st.pyplot(fig)
+                st.video(video_file)
+
+    elif choice == 'About':
+        st.subheader("About Green Cover Detection App")
+    
+    elif choice == "Metrics" :
+        figs = os.listdir("./figs")
+        names = ["loss-curve" , "confusion matrix" , "ROC_AUC curve" ,"Accuracy" , "F1" , "Precision" , "Recall"]
+        for fig , name in zip(figs , names) :
+            st.text(name)
+            st.image(os.path.join("./figs",fig))
+
+
+if __name__ == '__main__':
+    main()
